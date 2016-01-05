d23c390e595c2afd6325549e57a88dd5b5eadc9b
Adam Hoddinott <adam.hoddinott@salespreso.com>
2016-01-05 15:44:53 +1100

    docs(decorator): update to esdoc notation

--- src/lib/decorator.js
+++ src/lib/decorator.js
@@ -1,31 +1,16 @@
-/**
- * Decorator to use on React classes for easy auto
- * injection of form helpers (state/validation)
- * @module react-forms
- * @example
- ```javascript
- import form from "sp-react-forms/decorator";
- ```
- * @class decorators
- */
 import Hoc from "./Hoc";
 
 /**
- * @property default
+ * Decorator to use on React classes for easy auto
+ * injection of form helpers (state/validation)
+ * @returns {Function}
  * @example
-
-Note to remove the '\' in front of the decorator syntax
-
-```javascript
-import form from "sp-react-forms/decorator";
-
-\@form("yourForm")
-class MyForm extends React.Component {
-
-}
-```
+ * @form("yourForm")
+ * class MyForm extends React.Component {
+ *  // ...
+ * }
  */
-export default function(name) {
+export default function decorator(name) {
 	return function(Component) {
 		return Hoc(Component, name);
 	};
